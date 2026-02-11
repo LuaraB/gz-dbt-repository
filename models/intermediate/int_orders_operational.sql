b8d7b5cde003dbf205d3cb2d2eb5fe52505e9dbc
Luara Bühler <luara_buehler@hotmail.com>
2026-02-11 14:30:43 +0000

    added all tables, one by one

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,6 +1,15 @@
 select 
-    s.*,
-    sm.*,
+    s.orders_id,
+    s.shipping_fee,
+    s.logcost,
+    s.ship_cost,
+    sm.products_id,
+    sm.revenue,
+    sm.quantity,
+    sm.products_id,
+    sm.purchase_price,
+    sm.margin,
+    sm.purchase_cost,
     ROUND((sm.margin + s.shipping_fee - s.logcost - s.ship_cost),2) as operational_margin
 from {{ ref('stg_gz_raw_data__ship') }} s
 right join {{ ref('int_sales_margin') }} sm
