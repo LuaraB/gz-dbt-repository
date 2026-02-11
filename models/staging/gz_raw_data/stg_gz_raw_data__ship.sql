bb849891d7c84303680b5f1ab145eb7b9eee4a45
Luara Bühler <luara_buehler@hotmail.com>
2026-02-11 11:58:00 +0000

    further changes, including freshness test

--- models/staging/gz_raw_data/stg_gz_raw_data__ship.sql
+++ models/staging/gz_raw_data/stg_gz_raw_data__ship.sql
@@ -11,8 +11,9 @@ renamed as (
     select
         orders_id,
         shipping_fee,
+        --shipping_fee_1, deleted because its the same of shipping_fee
         logcost,
-        CAST(ship_cost AS FLOAT64) AS ship_cost
+        CAST(ship_cost AS FLOAT64) AS ship_cost 
     from source
     --WHERE shipping_fee <> shipping_fee_1
 )
